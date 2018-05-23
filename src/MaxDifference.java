27c2061f09f3720199be53a950fc369b7b253e04
sahil <sahil311289@gmail.com>
2018-05-23 00:33:53 -0700

    Reverse words in a string

---
+++ src/MaxDifference.java
@@ -0,0 +1,26 @@
+public class MaxDifference {
+    public static void main(String[] args) {
+        int[] ar = {4, 1, 2, 3};
+        maxDiff(ar);
+    }
+
+    static void maxDiff(int[] ar) {
+        int[] ar2 = new int[ar.length];
+
+        for (int i = 0; i < ar.length; i++) {
+
+            int maxDiff = -1;
+
+            for (int j = 0; j < i; j++) {
+                if (i > j && ar[i] > ar[j] && maxDiff < ar[i] - ar[j]) {
+                    maxDiff = ar[i] - ar[j];
+                }
+            }
+            ar2[i] = maxDiff;
+        }
+
+        for (int i : ar2) {
+            System.out.print(i + " ");
+        }
+    }
+}
